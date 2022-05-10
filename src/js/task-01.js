fbdf62fe80a0fcf4dbf391a2aa61cbe848f6f07d
Yevhen Podoliaka <zanp85740@gmail.com>
2022-05-10 16:24:06 +0300

    task1

---
+++ src/js/task-01.js
@@ -0,0 +1,11 @@
+const list = document.querySelector("#categories");
+const items = list.children;
+
+console.log(`Number of categories: ${items.length}`);
+
+for (let item of items) {
+  const title = item.querySelector("h2");
+  console.log(`Category : ${title.textContent}`);
+  const element = item.querySelectorAll("li");
+  console.log(`Elements : ${element.length}`);
+}
