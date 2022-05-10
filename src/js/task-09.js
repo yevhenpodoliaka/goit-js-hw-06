3aaf20d422356e2d2b5cc3efc252808f819561eb
Yevhen Podoliaka <zanp85740@gmail.com>
2022-05-10 18:04:56 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
+const buttonEl = document.querySelector(".change-color");
+
+buttonEl.addEventListener("click", changeBodyColor);
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
