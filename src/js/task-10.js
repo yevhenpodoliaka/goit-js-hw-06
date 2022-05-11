60f15f9e2dec1341098c182036279909c12c9c6e
Yevhen Podoliaka <zanp85740@gmail.com>
2022-05-11 15:04:40 +0300

    task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,37 @@
+const inputEl = document.querySelector("#controls > input");
+const boxesEl = document.querySelector("#boxes");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestory = document.querySelector("[data-destroy]");
+
+inputEl.addEventListener("input", null);
+btnCreate.addEventListener("click", makeBox);
+btnDestory.addEventListener("click", clearAll);
+
+function makeBox() {
+  createBoxes(inputEl.value); 
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  const array = [];
+  for (let i = 0; i < amount; i += 1) {
+    const item = document.createElement("div");
+    size += 10;
+    item.style.width = `${size}px`;
+    item.style.height = ` ${size}px`;
+    item.style.backgroundColor = getRandomHexColor();
+
+    array.push(item);
+  }
+  boxesEl.append(...array);
+}
+function clearAll() {
+  boxesEl.innerHTML = "";
+  inputEl.value = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
