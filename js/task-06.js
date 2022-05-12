bfd01819b0ad4d28f93c6a3bbf131bb97cf5f3dc
Yevhen Podoliaka <zanp85740@gmail.com>
2022-05-12 10:39:21 +0300

    refinement hw

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,18 @@ const inputEl = document.querySelector("#validation-input");
 const minInputLength = inputEl.dataset.length;
 
 inputEl.addEventListener("blur", onInputBlur);
-
-
+inputEl.addEventListener("focus", defoltStyle);
+function defoltStyle() {
+      inputEl.classList.remove("invalid");
+      inputEl.classList.remove("valid");
+    
+}
 
 function onInputBlur(event) {
   if (event.currentTarget.value.length < minInputLength) {
-    inputEl.classList.add("invalid");
-  } else inputEl.classList.remove("invalid"), inputEl.classList.add("valid");
+      inputEl.classList.add("invalid");
+      alert(`Ведіть не менше ${minInputLength} символів`);
+  } else inputEl.classList.add("valid");
 }
+
+
